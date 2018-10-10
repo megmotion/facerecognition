47110fcdb0eaabab784d0cd179f2bbfa5d553383
megmotion <33497517+megmotion@users.noreply.github.com>
2018-10-10 15:42:53 +0200

    updating database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex ({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-solid-90214',
     user : 'postgres',
     password : '123',
     database : 'smartbrain'
